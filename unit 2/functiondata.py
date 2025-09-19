2c47c8ca319a3a7dbd1e35327da06c96a21643eb
Jordan Wells <wellsj888@boyslatin.net>
2025-09-19 14:22:14 +0000

    Class assingment

--- unit 2/functiondata.py
+++ unit 2/functiondata.py
@@ -1,22 +1,22 @@
 # Howto create a fuction that passes data
 
 # Step 1. create a function definition
-#def bnbRefung(username, refundAmount):
-    #print('soryy ' + username + 'for you cancellation')
-    #print('We wll refund $' + str(refundAmount)+ 'amout yto your bank')
+def bnbRefung(username, refundAmount):
+    print('Sorry ' + username + ' for your cancellation')
+    print('We will refund $' + str(refundAmount) + ' amount to your bank')
+bnbRefung( 'Mary',5)
+#                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       Step 2. Call the function/ execute instructions
 
-# Step 2. Call the function/ execute instructions
-#bnbRefung( 'Mary',5)
 
-#def brithdy(name, date):
-    #print('My name is' + name + date)
+def birthday(name, date):
+    print('My name is' + name + date)
 
-#brithdy(' Jordan Wells ',' and my birthday is December 27. ')
+birthday(' Jordan Wells ',' and my birthday is December 27. ')
 
-#def dollarConvert(dollar):
-    #penneis = dollar * 100
-   #print ('My' + str(dollar) + ' dollar(s) is equal to' + str(pennies) + 'pennies')
-#dollarConvert(13)
+def dollarConvert(dollar):
+    pennies = dollar * 100
+    print ('My' + str(dollar) + ' dollar(s) is equal to' + str(pennies) + 'pennies')
+dollarConvert(13)
 
 def triangleArea(width, length):
     area = 0.5 * width * length
@@ -34,3 +34,16 @@ def temperatureConvert(fahrenheit):
     celsius = (fahrenheit * 5/9) - 32
     print (str(celsius) +  ' degrees celsius is equal to.' + str(fahrenheit) + ' degrees fahrenheit.')
 temperatureConvert(100)
+
+def addition(num1, num2):
+    sum = num1 + num2
+    print ('The sum of ' + str(num1) + ' + ' + str(num2) + ' = ' + str(sum))
+addition(20, 30)
+
+def multiply(num1):
+    num2 = input('please type in a number: ')
+    sum = num1 * int(num2)
+    print(sum)
+    print(str(num1) + "* " + num2 + "= " + str(sum))
+
+multiply(10)
