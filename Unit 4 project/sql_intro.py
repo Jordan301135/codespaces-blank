c441fb8d8763f4afbb9a85962061e6094bfe8b71
Jordan Wells <wellsj888@boyslatin.net>
2026-04-30 13:44:36 +0000

    SQL

---
+++ Unit 4 project/sql_intro.py
@@ -0,0 +1,45 @@
+# 1. Bring in the spl lite module to get access to all the sql methods(function) to work with our database.
+import sqlite3
+
+# 2. We need to established an actual database file using the connect method.
+connect = sqlite3.connect('myDb.db')
+
+# 3. Create a object that will be sent to the new database.
+cursor = connect.cursor()
+
+# 4. We need to create a schema (structure) for our data.
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS gameSales (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        platform TEXT,
+        developer TEXT,
+        price INTEGER,
+        genre TEXT,
+        total_price INTEGER
+    )
+''')
+# 5. We can now create or first database object.
+# cursor.execute('''
+#                INSERT INTO gameSales (name, platform, developer, price, genre, total_price)
+#                VALUES ('Crimson Desert', 'Ps5', 'Perals Abyss', 70, 'Action/Adventure', 5000000)
+#                ''')
+
+cursor.execute('''
+               INSERT INTO gameSales (name, platform, developer, price, genre, total_price)
+               VALUES ('Resident Evil', 'Xbox', 'Capcom', 80, 'horror', 800000000)
+               ''')
+               
+
+
+
+# cursor.execute('''
+#                INSERT INTO gameSales (name, platform, developer, price, genre, total_price)
+#                VALUES ('Super Smash Bros Ultimate', 'Nintendo Switch', 'Bandai Namco Studios', 25, 'Fighting', 36240000)
+#                ''')
+
+
+# 6. once we have a new data object, we can commit this to our database.
+
+connect.commit()
+connect.close()
