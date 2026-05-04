cc5e273c69b83d546b306337290b383fba9f354d
Jordan Wells <wellsj888@boyslatin.net>
2026-05-04 14:30:21 +0000

    SQL QUERY

--- Unit 4 project/SqlQuery.py
+++ Unit 4 project/SqlQuery.py
@@ -1,43 +1,68 @@
 # import all the methods we will need to send/ receieve data from DB
 import sqlite3
 
-# just function
-def getALLDATA():
-    # setting up the connection to our dd (the road)
+# # just function
+# def getALLDATA():
+#     # setting up the connection to our dd (the road)
+#     connect = sqlite3.connect('myDb.db')
+
+#     #setting up or vehicle to send/ receive data
+#     # to / from the DB (the vehicle)
+#     cursor = connect.cursor()
+
+#     # the data we want to get
+#     # SELECT = ouyr selection
+#     query = "SELECT genre FROM gameSales"
+
+#     # turns the vehicle on to the data
+#     cursor.execute(query)
+
+#     # tells cursor to fetch/ get ALL data
+#     results = cursor.fetchall()
+
+#     # show our results in terminal
+#     print(results)
+
+# # call function
+# getALLDATA()
+
+
+# def get1data():
+#     connect = sqlite3.connect('myDb.db')
+
+#     cursor = connect.cursor()
+
+#     query = "SELECT genre FROM gameSales WHERE developer = 'Bandai'"
+
+#     cursor.execute(query)
+
+#     results = cursor.fetchone()
+
+#     print(results)
+
+# get1data()
+
+
+
+def deleteData():
     connect = sqlite3.connect('myDb.db')
 
-    #setting up or vehicle to send/ receive data
-    # to / from the DB (the vehicle)
     cursor = connect.cursor()
 
-    # the data we want to get
-    # SELECT = ouyr selection
-    query = "SELECT genre FROM gameSales"
+    # Correct SQL syntax
+    query = "DELETE FROM gameSales WHERE developer = ?"
 
-    # turns the vehicle on to the data
-    cursor.execute(query)
+    # Execute with parameter
+    cursor.execute(query, ("Capcom",))
 
-    # tells cursor to fetch/ get ALL data
-    results = cursor.fetchall()
+    # Save changes
+    connect.commit()
 
-    # show our results in terminal
-    print(results)
+    print("Data deleted successfully.")
 
-# call function
-getALLDATA()
+    connect.close()
+
+deleteData()
 
 
-def get1data():
-    connect = sqlite3.connect('myDb.db')
-
-    cursor = connect.cursor()
-
-    query = "SELECT genre FROM gameSales WHERE developer = 'Bandai'"
-
-    cursor.execute(query)
-
-    results = cursor.fetchone()
-
-    print(results)
-
-get1data()
+# create a function that will delete an object from the database
